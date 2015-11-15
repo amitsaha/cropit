bda451cc528c7aaf97ea32dbaf752d845a5bea8a
Amit Saha <amitsaha.in@gmail.com>
2015-11-16 08:43:37 +1100

    cropit: gofmt-ed and README update

--- README.md
+++ README.md
@@ -3,5 +3,5 @@ A simple command line program to crop images
 
 # Using it
 
-go get github.com/oliamb/cutter
-go run main.go <image>
+- go get github.com/oliamb/cutter
+- go run main.go <image>
--- main.go
+++ main.go
@@ -1,30 +1,40 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"os"
-	"github.com/oliamb/cutter"
 	"image"
 	"image/jpeg"
+	"image/png"
 	"log"
+	"os"
+	"path/filepath"
+	"github.com/oliamb/cutter"
 )
 
 func main() {
 
-	f, err := os.Open(os.Args[1])
+	// Open the file specified as the first argument
+	inputFilePath := os.Args[1]
+	inputFileDir, inputFileName := filepath.Split(inputFilePath)
+	f, err := os.Open(inputFilePath)
 	if err != nil {
 		log.Fatal("Cannot open file", err)
 	}
 	defer f.Close()
-	
+
+	// We first decode the image
 	img, _, err := image.Decode(f)
 	if err != nil {
 		log.Fatal("Cannot decode image:", err)
 	}
 
-	cImg, err := cutter.Crop(img, cutter.Config{
-		Height:  1000, 
-		Width:   1000, 
+	// Perform the cropping
+	cHeight := 800
+	cWidth := 700
+	croppedImg, err := cutter.Crop(img, cutter.Config{
+		Height:  cHeight,
+		Width:   cWidth,
 		Mode:    cutter.TopLeft,
 		Anchor:  image.Point{60, 10},
 		Options: 0,
@@ -35,14 +45,27 @@ func main() {
 	}
 
 	// Write the cropped image into a file
-	cropped_f, err := os.Create("cropped.jpg")
+	croppedFileName := "cropped_" + inputFileName
+	croppedFilePath := filepath.Join(inputFileDir, croppedFileName)
+	croppedFile, err := os.Create(croppedFilePath)
 	if err != nil {
 		log.Fatal("Could not save cropped image")
 	}
-	defer cropped_f.Close()
-	err = jpeg.Encode(cropped_f, cImg, &jpeg.Options{})
+	defer croppedFile.Close()
+
+	// Now we encode the cropped image data using the appropriate
+	// encoder and save it to the above file
+	switch filepath.Ext(inputFileName) {
+	case ".png":
+		err = png.Encode(croppedFile, croppedImg)
+	case ".jpg":
+		err = jpeg.Encode(croppedFile, croppedImg, &jpeg.Options{})
+	default:
+		err = errors.New("Unsupported format: " + filepath.Ext(inputFileName))
+	}
+
 	if err != nil {
 		log.Fatal("Error saving")
 	}
-	fmt.Println("Saved cropped image as cropped.jpg")
+	fmt.Printf("Saved cropped image as %s\n", croppedFilePath)
 }
