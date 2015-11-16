0988edbdf92651870e52f6e95f99cfcc7cc4324f
Amit Saha <amitsaha.in@gmail.com>
2015-11-16 21:25:24 +1100

    cropit: add flag validation

--- main.go
+++ main.go
@@ -13,17 +13,14 @@ import (
 	"github.com/oliamb/cutter"
 )
 
-func main() {
+func cropAndSave(inputFilePath string, cWidth int, cHeight int) () {
 
-	// Setup the flags
-	cHeight := flag.Int("height", 0, "Crop Height")
-	cWidth := flag.Int("width", 0, "Crop Width")
-	flag.Parse()
-
-	//XX: Check flags, file name specified
-
-	// Open the file specified as the first argument
-	inputFilePath := flag.Args()[0]
+	// Check if we can handle this image
+	fileExtension := filepath.Ext(inputFilePath)
+	if fileExtension != ".jpg" && fileExtension != ".png" {
+		log.Fatal("Cannot handle image of this type")
+	}
+	
 	inputFileDir, inputFileName := filepath.Split(inputFilePath)
 	f, err := os.Open(inputFilePath)
 	if err != nil {
@@ -39,8 +36,8 @@ func main() {
 
 	// Perform the cropping
 	croppedImg, err := cutter.Crop(img, cutter.Config{
-		Height:  *cHeight,
-		Width:   *cWidth,
+		Height:  cHeight,
+		Width:   cWidth,
 		Mode:    cutter.TopLeft,
 		Anchor:  image.Point{60, 10},
 		Options: 0,
@@ -61,7 +58,7 @@ func main() {
 
 	// Now we encode the cropped image data using the appropriate
 	// encoder and save it to the above file
-	switch filepath.Ext(inputFileName) {
+	switch fileExtension {
 	case ".png":
 		err = png.Encode(croppedFile, croppedImg)
 	case ".jpg":
@@ -74,4 +71,29 @@ func main() {
 		log.Fatal("Error saving")
 	}
 	fmt.Printf("Saved cropped image as %s\n", croppedFilePath)
+}
+
+func main() {
+
+	// Setup the flags
+	cHeight := flag.Int("height", 0, "Crop Height")
+	cWidth := flag.Int("width", 0, "Crop Width")
+	flag.Parse()
+
+	//Check flags, file name specified
+	if *cHeight == 0 {
+		log.Fatal("Must specify the Crop Height")
+	}
+
+	if *cWidth == 0 {
+		log.Fatal("Must specify crop width")
+	}
+
+	if len(flag.Args()) == 0 {
+		log.Fatal("Must specify at least 1 image to crop")
+	}
+	
+	// Open the file specified as the first argument
+	inputFilePath := flag.Args()[0]
+	cropAndSave(inputFilePath, *cWidth, *cHeight)
 }
