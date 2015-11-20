636823dba2c84b9500cff1db55276328ff7765ba
Amit Saha <amitsaha.in@gmail.com>
2015-11-21 10:32:32 +1100

    Making crop() more generic

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 
 import (
 	"bytes"
+	"bufio"
 	"flag"
 	"fmt"
 	"github.com/oliamb/cutter"
@@ -10,16 +11,28 @@ import (
 	"image/jpeg"
 	"image/png"
 	"io/ioutil"
+	"io"
 	"log"
+	"net/http"
 	"os"
 	"path/filepath"
 )
 
-// Return the cropped Image
-func crop(imageData []byte, imageType string, cWidth int, cHeight int) image.Image {
+func validateImageType(image_type string) bool {
+	recognizedImageTypes := []string{"image/jpeg", "image/png"}
+	for _, t := range recognizedImageTypes {
+		if image_type == t {
+			return true
+		}
+	}
+	return false
+}
+
+// Crop the image and write the cropped image to the provided writer
+func crop(imageData []byte, imageType string, cWidth int, cHeight int, w io.Writer)  {
 
 	// Check if we can handle this image
-	if imageType != ".jpg" && imageType != ".png" {
+	if !validateImageType(imageType) {
 		log.Fatal("Cannot handle image of this type")
 	}
 
@@ -43,7 +56,17 @@ func crop(imageData []byte, imageType string, cWidth int, cHeight int) image.Ima
 		log.Fatal("Cannot crop image:", err)
 	}
 
-	return croppedImg
+	// Now we encode the cropped image data using the appropriate
+	// encoder and save it to the above file
+	switch imageType {
+	case "image/png":
+		err = png.Encode(w, croppedImg)
+	case "image/jpeg":
+		err = jpeg.Encode(w, croppedImg, &jpeg.Options{})
+	}
+	if err != nil {
+		log.Fatal("Error saving")
+	}
 }
 
 func main() {
@@ -67,35 +90,28 @@ func main() {
 	}
 
 	// Loop over each file , crop and save the cropped image.
-	for i, inputFilePath := range flag.Args() {
+	for _, inputFilePath := range flag.Args() {
 		imageData, err := ioutil.ReadFile(inputFilePath)
 		if err != nil {
 			log.Fatal("Cannot open file", err)
 		}
-
-		// Open the file specified as the first argument
-		imageType := filepath.Ext(inputFilePath)
-		croppedImg := crop(imageData, imageType, *cWidth, *cHeight)
-
+		
 		// Write the cropped image into a file
-		croppedFileName := fmt.Sprintf("cropped_%d%s", i, imageType)
+		croppedFileDir, fileName := filepath.Split(inputFilePath)
+		croppedFileName := fmt.Sprintf("%scropped_%s", croppedFileDir, fileName)
 		croppedFile, err := os.Create(croppedFileName)
 		if err != nil {
 			log.Fatal("Could not create file for cropped image")
 		}
+		
 		defer croppedFile.Close()
 
-		// Now we encode the cropped image data using the appropriate
-		// encoder and save it to the above file
-		switch imageType {
-		case ".png":
-			err = png.Encode(croppedFile, croppedImg)
-		case ".jpg":
-			err = jpeg.Encode(croppedFile, croppedImg, &jpeg.Options{})
-		}
+		croppedFileWriter := bufio.NewWriter(croppedFile)
+		// Detect file type and call the crop() function
+		imageType := http.DetectContentType(imageData)
+		crop(imageData, imageType, *cWidth, *cHeight, croppedFileWriter)
 
-		if err != nil {
-			log.Fatal("Error saving")
-		}
+		// Flush
+		croppedFileWriter.Flush()
 	}
 }
